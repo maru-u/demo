32ef797f8b0366a9197fc456fac3c540edcf5d31
maru <takuji.funao@gmail.com>
2019-02-03 12:19:59 +0900

    Initial Commit

---
+++ Gululi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gululi
+//
+//  Created by 舩尾拓史 on 2019/02/03.
+//  Copyright © 2019年 gululi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
